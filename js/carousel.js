3b32aa6961d05be60968b52b5599522b8b75ca6d
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2022-03-04 14:02:54 -0400

    feito a primeira parte, falta o comportamento

---
+++ js/carousel.js
@@ -0,0 +1,8 @@
+const track = document.querySelector('.carousel-track')
+const slides = Array.from(track.children)
+
+console.log(slides)
+
+//when I click left, move slides to the left
+//when I click right, move slides to the right
+//when I click the nav indicators, move to that slide
