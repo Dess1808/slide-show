f9c94cff9f42dfb14baa5ecce62efd272c8a420d
Gabriel Martins <gabrielmartins0898@gmail.com>
2022-03-05 14:02:29 -0400

    adicionando um pouco mais de comportamento no slide, falta finalizar

--- js/carousel.js
+++ js/carousel.js
@@ -1,22 +1,29 @@
 const track = document.querySelector('.carousel-track')
 const slides = Array.from(track.children)
-const nexButton = document.querySelector('.carousel-button-right')
-const prevButton = document.querySelector('.carousel-button-left')
+const nexButton = document.querySelector('.carousel-button--right')
+const prevButton = document.querySelector('.carousel-button--left')
 const dotsNav = document.querySelector('.carousel-nav')
 const dots = Array.from(dotsNav.children)
 
 const slideWidth = slides[0].getBoundingClientRect().width
 
 //arrange the slides next to one another
-// slides[0].style.left = slideWidth * 0 + 'px'
-// slides[1].style.left = slideWidth * 1 + 'px'
-// slides[2].style.left = slideWidth * 2 + 'px'
 
-slides.forEach((slide, index) =>{
+const setSlidePosition = (slide, index) => {
     slide.style.left = slideWidth * index + 'px'
-})
+}
+
+slides.forEach(setSlidePosition)
 
 //when I click left, move slides to the left
 
 //when I click right, move slides to the right
+nexButton.addEventListener('click', e => {
+    const currentSlide = track.querySelector('.current-slide')
+    const nextSlide = currentSlide.nextElementSibling
+    console.log(currentSlide.nextElementSibling)
+    const amoutToMove = nextSlide.style.left
+   //move to next slide
+   track.style.transform = 'translatex(-' + amoutToMove + ')'
+})
 //when I click the nav indicators, move to that slide
