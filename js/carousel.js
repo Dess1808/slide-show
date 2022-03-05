51b0b03f7074076dd934b2d195d58c0eed16f8aa
Gabriel Martins <gabrielmartins0898@gmail.com>
2022-03-04 23:32:27 -0400

    adicionado o inicio do js para o slide-show

--- js/carousel.js
+++ js/carousel.js
@@ -1,8 +1,22 @@
 const track = document.querySelector('.carousel-track')
 const slides = Array.from(track.children)
+const nexButton = document.querySelector('.carousel-button-right')
+const prevButton = document.querySelector('.carousel-button-left')
+const dotsNav = document.querySelector('.carousel-nav')
+const dots = Array.from(dotsNav.children)
 
-console.log(slides)
+const slideWidth = slides[0].getBoundingClientRect().width
+
+//arrange the slides next to one another
+// slides[0].style.left = slideWidth * 0 + 'px'
+// slides[1].style.left = slideWidth * 1 + 'px'
+// slides[2].style.left = slideWidth * 2 + 'px'
+
+slides.forEach((slide, index) =>{
+    slide.style.left = slideWidth * index + 'px'
+})
 
 //when I click left, move slides to the left
+
 //when I click right, move slides to the right
 //when I click the nav indicators, move to that slide
