7948a4a395e0aefa6979c6de0e2f666e771d0001
Gabriel Martins <gabrielmartins0898@gmail.com>
2022-03-06 20:48:01 -0400

    slide completo, falta apenas os pontos

--- js/carousel.js
+++ js/carousel.js
@@ -15,15 +15,42 @@ const setSlidePosition = (slide, index) => {
 
 slides.forEach(setSlidePosition)
 
+const moveToSlide = (track, currentSlide, targetSlide) =>{
+    track.style.transform = 'translatex(-' + targetSlide.style.left + ')'
+    currentSlide.classList.remove('current-slide')
+    targetSlide.classList.add('current-slide')
+}
+
 //when I click left, move slides to the left
+prevButton.addEventListener('click', e =>{
+    const currentSlide = track.querySelector('.current-slide')
+    const prevSlide = currentSlide.previousElementSibling
+
+    moveToSlide(track, currentSlide, prevSlide)
+})
 
 //when I click right, move slides to the right
 nexButton.addEventListener('click', e => {
     const currentSlide = track.querySelector('.current-slide')
     const nextSlide = currentSlide.nextElementSibling
-    console.log(currentSlide.nextElementSibling)
-    const amoutToMove = nextSlide.style.left
-   //move to next slide
-   track.style.transform = 'translatex(-' + amoutToMove + ')'
+
+    moveToSlide(track, currentSlide, nextSlide)
 })
+
 //when I click the nav indicators, move to that slide
+dotsNav.addEventListener('click', e =>{
+    //what indicator was clicked on?
+    const targetDot = e.target.closest('button')
+
+    if(!targetDot) return
+    
+    const currentSlide = track.querySelector('.current-slide')
+    const currentDot = track.querySelector('.current-slide')
+    const targetIndex = dots.findIndex(dots => dots === targetDot)
+    const targetSlide = slides[targetIndex]
+    
+
+    moveToSlide(track, currentSlide, targetSlide)
+})
+
+//automatic slide show
